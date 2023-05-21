b6c7c360b8bf82dd717f5d94ba22f4fdc2152b82
Mathéo DEBAL <71602905+Tsyke@users.noreply.github.com>
2023-05-21 21:32:42 +0200

    Delete v13 directory

--- v13/Functions.js
+++
@@ -1,6 +0,0 @@
-const { deleteCommands, loadCommands, loadEvents } = require("./AppFunctions")
-
-module.exports.AllCommand = async (Client) => {
-    loadCommands(Client);
-    loadEvents(Client);
-}
